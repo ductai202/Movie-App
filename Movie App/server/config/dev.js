163141720123290ff7cf51d99ad12731fd866651
ductai202 <70333438+ductai202@users.noreply.github.com>
2021-07-02 14:23:55 +0700

    movies app

---
+++ Movie App/server/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://ductai202:1q2w3e@cluster0.orneh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Setup  :
+
+### 1. cd .\react-tmdb-app\
+### 2. npm install
+### 3. cd .\client\
+### 4. npm install
+### 5. cd ..
+### 6. npm run dev 
+
